fac98e32d52aaf87d6766f06c393005def1071df
naveed.iqbal <naveed.iqbal@ihsinformatics.com>
2020-03-25 21:02:53 +0500

    aefadf

--- api/src/main/java/org/openmrs/module/hydra/api/form_service/FormService.java
+++ api/src/main/java/org/openmrs/module/hydra/api/form_service/FormService.java
@@ -354,58 +354,52 @@ public class FormService {
 							}
 
 							// Creating a unique value group id
-							Calendar c = Calendar.getInstance();
-							int valueGroupId = (c.get(Calendar.YEAR) + c.get(Calendar.MONTH) + c.get(Calendar.DAY_OF_MONTH)
-							        + c.get(Calendar.HOUR_OF_DAY) + c.get(Calendar.MINUTE) + c.get(Calendar.SECOND)
-							        + c.get(Calendar.MILLISECOND) + (i + 1)) * (j + 2);
-
-							Concept questionConceptDOB = conceptService
-							        .getConceptByUuid("160751AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-							Concept questionConceptGender = conceptService
-							        .getConceptByUuid("75a17321-fd69-47be-831e-cd773dc9c3cc");
-							Concept questionConceptGivenName = conceptService
-							        .getConceptByUuid("85fc8b27-8a08-4bdc-bf91-e34af50265aa");
-							Concept questionConceptFamilyName = conceptService
-							        .getConceptByUuid("82c65f7c-b9e8-4775-a8bd-f7ad55ab8bf0");
-							Concept questionConceptIdentifier = conceptService
-							        .getConceptByUuid("a24d649b-fb89-4b8c-beeb-aacf2872cf22");
-							Concept questionConceptRelationship = conceptService
-							        .getConceptByUuid("7579f93d-ebe7-423f-822b-dbe792248499");
-
-							Obs obsDOB = new Obs();
-							Obs obsGender = new Obs();
-							Obs obsGivenName = new Obs();
-							Obs obsFamilyName = new Obs();
-							Obs obsIdentifier = new Obs();
-							Obs obsRelationship = new Obs();
-
-							obsDOB.setValueGroupId(valueGroupId);
-							obsGender.setValueGroupId(valueGroupId);
-							obsGivenName.setValueGroupId(valueGroupId);
-							obsFamilyName.setValueGroupId(valueGroupId);
-							obsIdentifier.setValueGroupId(valueGroupId);
-							obsRelationship.setValueGroupId(valueGroupId);
-
-							obsDOB.setConcept(questionConceptDOB);
-							obsGender.setConcept(questionConceptGender);
-							obsGivenName.setConcept(questionConceptGivenName);
-							obsFamilyName.setConcept(questionConceptFamilyName);
-							obsIdentifier.setConcept(questionConceptIdentifier);
-							obsRelationship.setConcept(questionConceptRelationship);
-
-							obsDOB.setValueDate(birthDate);
-							obsGender.setValueText(gender);
-							obsGivenName.setValueText(givenName);
-							obsFamilyName.setValueText(familyName);
-							obsIdentifier.setValueText(identifier);
-							obsRelationship.setValueText(relationship);
-
-							obsList.add(obsDOB);
-							obsList.add(obsGender);
-							obsList.add(obsGivenName);
-							obsList.add(obsFamilyName);
-							obsList.add(obsIdentifier);
-							obsList.add(obsRelationship);
+							/*
+							 * Calendar c = Calendar.getInstance(); int valueGroupId = (c.get(Calendar.YEAR)
+							 * + c.get(Calendar.MONTH) + c.get(Calendar.DAY_OF_MONTH) +
+							 * c.get(Calendar.HOUR_OF_DAY) + c.get(Calendar.MINUTE) + c.get(Calendar.SECOND)
+							 * + c.get(Calendar.MILLISECOND) + (i + 1)) * (j + 2);
+							 * 
+							 * Concept questionConceptDOB = conceptService
+							 * .getConceptByUuid("160751AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"); Concept
+							 * questionConceptGender = conceptService
+							 * .getConceptByUuid("75a17321-fd69-47be-831e-cd773dc9c3cc"); Concept
+							 * questionConceptGivenName = conceptService
+							 * .getConceptByUuid("85fc8b27-8a08-4bdc-bf91-e34af50265aa"); Concept
+							 * questionConceptFamilyName = conceptService
+							 * .getConceptByUuid("82c65f7c-b9e8-4775-a8bd-f7ad55ab8bf0"); Concept
+							 * questionConceptIdentifier = conceptService
+							 * .getConceptByUuid("a24d649b-fb89-4b8c-beeb-aacf2872cf22"); Concept
+							 * questionConceptRelationship = conceptService
+							 * .getConceptByUuid("7579f93d-ebe7-423f-822b-dbe792248499");
+							 * 
+							 * Obs obsDOB = new Obs(); Obs obsGender = new Obs(); Obs obsGivenName = new
+							 * Obs(); Obs obsFamilyName = new Obs(); Obs obsIdentifier = new Obs(); Obs
+							 * obsRelationship = new Obs();
+							 * 
+							 * obsDOB.setValueGroupId(valueGroupId);
+							 * obsGender.setValueGroupId(valueGroupId);
+							 * obsGivenName.setValueGroupId(valueGroupId);
+							 * obsFamilyName.setValueGroupId(valueGroupId);
+							 * obsIdentifier.setValueGroupId(valueGroupId);
+							 * obsRelationship.setValueGroupId(valueGroupId);
+							 * 
+							 * obsDOB.setConcept(questionConceptDOB);
+							 * obsGender.setConcept(questionConceptGender);
+							 * obsGivenName.setConcept(questionConceptGivenName);
+							 * obsFamilyName.setConcept(questionConceptFamilyName);
+							 * obsIdentifier.setConcept(questionConceptIdentifier);
+							 * obsRelationship.setConcept(questionConceptRelationship);
+							 * 
+							 * obsDOB.setValueDate(birthDate); obsGender.setValueText(gender);
+							 * obsGivenName.setValueText(givenName); obsFamilyName.setValueText(familyName);
+							 * obsIdentifier.setValueText(identifier);
+							 * obsRelationship.setValueText(relationship);
+							 * 
+							 * obsList.add(obsDOB); obsList.add(obsGender); obsList.add(obsGivenName);
+							 * obsList.add(obsFamilyName); obsList.add(obsIdentifier);
+							 * obsList.add(obsRelationship);
+							 */
 
 						}
 					}
