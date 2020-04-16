34cac078a0269b86980131273d86f47a9529471b
naveed.iqbal <h.naveediqbal@gmail.com>
2020-04-16 10:25:23 +0500

    Merged CSV export work from Rabbia

--- omod/src/main/java/org/openmrs/module/hydra/web/controller/ReportController.java
+++ omod/src/main/java/org/openmrs/module/hydra/web/controller/ReportController.java
@@ -1,21 +1,20 @@
 package org.openmrs.module.hydra.web.controller;
 
 import org.openmrs.api.context.Context;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.util.OpenmrsConstants;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.opencsv.CSVWriter;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
@@ -35,20 +34,29 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.InvalidParameterException;
 import java.util.Map.Entry;
-import org.springframework.core.io.Resource;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.SQLQuery;
+import org.hibernate.procedure.ProcedureCall;
+import org.hibernate.transform.AliasedTupleSubsetResultTransformer;
 
+import javax.persistence.ParameterMode;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -66,6 +74,9 @@ public class ReportController {
 	public static final String SQL_DATESTAMP = "yyyyMMdd";
 
 	private static final Map<String, String> DATE_FORMATS;
+
+	@Autowired
+	private DbSessionFactory sessionFactory;
 
 	static {
 		DATE_FORMATS = new HashMap();
@@ -86,14 +97,247 @@ public class ReportController {
 
 	}
 
+	@RequestMapping(value = "/dump/encounters", method = RequestMethod.GET)
+	@ResponseBody
+	public HttpEntity<byte[]> getEncounterDumps(HttpServletRequest request,
+			@RequestParam(value = "workflow", required = true) String workflow,
+			@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to) throws JRException, IOException {
+
+		String format = detectDateFormat(from);
+		Date sDate = fromString(from, format);
+
+		String format1 = detectDateFormat(to);
+		Date eDate = fromString(to, format1);
+
+		ProcedureCall call = sessionFactory.getCurrentSession().createStoredProcedureCall("deencountrised");
+
+		call.registerParameter(1, Date.class, ParameterMode.IN).bindValue(sDate);
+		call.registerParameter(2, Date.class, ParameterMode.IN).bindValue(eDate);
+		call.registerParameter(3, String.class, ParameterMode.IN).bindValue(workflow);
+		call.getOutputs().getCurrent();
+
+		SQLQuery sql = sessionFactory.getCurrentSession().createSQLQuery(
+				"select CONCAT('enc_',alphanum(LOWER(encounter_type.name))) from encounter_type where retired = 0");
+		List<String> encounterTables = sql.list();
+
+		String zipFile = System.getProperty("java.io.tmpdir") + File.separator + "encounters_dump.zip";
+		FileOutputStream fos = new FileOutputStream(zipFile);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		byte[] buffer = new byte[1024];
+
+		for (String encounterTable : encounterTables) {
+
+			sql = sessionFactory.getCurrentSession().createSQLQuery("select * from " + encounterTable);
+			sql.setResultTransformer(AliasToEntityOrderedMapResultTransformer.INSTANCE);
+			List<Map<String, Object>> aliasToValueMapList = sql.list();
+
+			if (!aliasToValueMapList.isEmpty()) {
+
+				String outputFile = System.getProperty("java.io.tmpdir") + File.separator + encounterTable + ".csv";
+				createCSVFileFromMapList(aliasToValueMapList, outputFile);
+
+				File srcFile = new File(outputFile);
+
+				FileInputStream fis = new FileInputStream(srcFile);
+
+				// begin writing a new ZIP entry, positions the stream to the start of the entry
+				// data
+				zos.putNextEntry(new ZipEntry(srcFile.getName()));
+
+				int length;
+
+				while ((length = fis.read(buffer)) > 0) {
+					zos.write(buffer, 0, length);
+				}
+
+				zos.closeEntry();
+
+				// close the InputStream
+				fis.close();
+
+			}
+
+		}
+
+		zos.close();
+
+		byte[] bytes = Files.readAllBytes(Paths.get(zipFile));
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+		header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "encounters_dump.zip");
+		header.setContentLength(bytes.length);
+
+		return new HttpEntity<byte[]>(bytes, header);
+
+	}
+
+	@RequestMapping(value = "/dump/patients", method = RequestMethod.GET)
+	@ResponseBody
+	public HttpEntity<byte[]> getPatientDumps(HttpServletRequest request,
+			@RequestParam(value = "workflow", required = true) String workflow,
+			@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to) throws JRException, IOException {
+
+		String format = detectDateFormat(from);
+		Date sDate = fromString(from, format);
+
+		String format1 = detectDateFormat(to);
+		Date eDate = fromString(to, format1);
+
+		ProcedureCall call = sessionFactory.getCurrentSession().createStoredProcedureCall("norm_patient");
+
+		call.registerParameter(1, Date.class, ParameterMode.IN).bindValue(sDate);
+		call.registerParameter(2, Date.class, ParameterMode.IN).bindValue(eDate);
+		call.registerParameter(3, String.class, ParameterMode.IN).bindValue(workflow);
+		call.getOutputs().getCurrent();
+
+		SQLQuery sql = sessionFactory.getCurrentSession().createSQLQuery("select * from norm_patient");
+		sql.setResultTransformer(AliasToEntityOrderedMapResultTransformer.INSTANCE);
+		List<Map<String, Object>> aliasToValueMapList = sql.list();
+		String outputFile = System.getProperty("java.io.tmpdir") + File.separator + "patients_dump.csv";
+		if (aliasToValueMapList.isEmpty())
+			new File(outputFile);
+		else
+			createCSVFileFromMapList(aliasToValueMapList, outputFile);
+
+		byte[] bytes = Files.readAllBytes(Paths.get(outputFile));
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+		header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "patients_dump.csv");
+		header.setContentLength(bytes.length);
+
+		return new HttpEntity<byte[]>(bytes, header);
+
+	}
+
+	@RequestMapping(value = "/dump/providers", method = RequestMethod.GET)
+	@ResponseBody
+	public HttpEntity<byte[]> getProviderDumps(HttpServletRequest request,
+			@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to) throws JRException, IOException {
+
+		String format = detectDateFormat(from);
+		Date sDate = fromString(from, format);
+
+		String format1 = detectDateFormat(to);
+		Date eDate = fromString(to, format1);
+
+		ProcedureCall call = sessionFactory.getCurrentSession().createStoredProcedureCall("norm_provider");
+
+		call.registerParameter(1, Date.class, ParameterMode.IN).bindValue(sDate);
+		call.registerParameter(2, Date.class, ParameterMode.IN).bindValue(eDate);
+		call.getOutputs().getCurrent();
+
+		SQLQuery sql = sessionFactory.getCurrentSession().createSQLQuery("select * from norm_provider");
+		sql.setResultTransformer(AliasToEntityOrderedMapResultTransformer.INSTANCE);
+		List<Map<String, Object>> aliasToValueMapList = sql.list();
+		String outputFile = System.getProperty("java.io.tmpdir") + File.separator + "providers_dump.csv";
+		if (aliasToValueMapList.isEmpty())
+			new File(outputFile);
+		else
+			createCSVFileFromMapList(aliasToValueMapList, outputFile);
+
+		byte[] bytes = Files.readAllBytes(Paths.get(outputFile));
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+		header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "providers_dump.csv");
+		header.setContentLength(bytes.length);
+
+		return new HttpEntity<byte[]>(bytes, header);
+
+	}
+
+	@RequestMapping(value = "/dump/locations", method = RequestMethod.GET)
+	@ResponseBody
+	public HttpEntity<byte[]> getLocationDumps(HttpServletRequest request,
+			@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to) throws JRException, IOException {
+
+		String format = detectDateFormat(from);
+		Date sDate = fromString(from, format);
+
+		String format1 = detectDateFormat(to);
+		Date eDate = fromString(to, format1);
+
+		ProcedureCall call = sessionFactory.getCurrentSession().createStoredProcedureCall("norm_location");
+
+		call.registerParameter(1, Date.class, ParameterMode.IN).bindValue(sDate);
+		call.registerParameter(2, Date.class, ParameterMode.IN).bindValue(eDate);
+		call.getOutputs().getCurrent();
+
+		SQLQuery sql = sessionFactory.getCurrentSession().createSQLQuery("select * from norm_location");
+		sql.setResultTransformer(AliasToEntityOrderedMapResultTransformer.INSTANCE);
+		List<Map<String, Object>> aliasToValueMapList = sql.list();
+		String outputFile = System.getProperty("java.io.tmpdir") + File.separator + "locations_dump.csv";
+		if (aliasToValueMapList.isEmpty())
+			new File(outputFile);
+		else
+			createCSVFileFromMapList(aliasToValueMapList, outputFile);
+
+		byte[] bytes = Files.readAllBytes(Paths.get(outputFile));
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+		header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + "locations_dump.csv");
+		header.setContentLength(bytes.length);
+
+		return new HttpEntity<byte[]>(bytes, header);
+
+	}
+
+	public void createCSVFileFromMapList(List<Map<String, Object>> aliasToValueMapList, String outputFile) {
+
+		File file = new File(outputFile);
+
+		try {
+			FileWriter outputfile = new FileWriter(file);
+
+			CSVWriter writer = new CSVWriter(outputfile);
+
+			// adding header to csv
+			Map<String, Object> extractHeader = aliasToValueMapList.get(0);
+			String[] header = new String[extractHeader.size()];
+			int i = 0;
+			for (Map.Entry<String, Object> entry : extractHeader.entrySet()) {
+				header[i++] = entry.getKey();
+			}
+			writer.writeNext(header);
+
+			// add data to csv
+			for (Map<String, Object> maps : aliasToValueMapList) {
+				Object[] data = new Object[extractHeader.size()];
+				i = 0;
+				for (Map.Entry<String, Object> entry : maps.entrySet()) {
+					data[i++] = entry.getValue();
+				}
+
+				String[] stringArray = new String[data.length];
+				for (int j = 0; j < data.length; j++)
+					stringArray[j] = String.valueOf(data[j]);
+
+				writer.writeNext(stringArray);
+
+			}
+
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public HttpEntity<byte[]> getReportByName(HttpServletRequest request,
-	        @RequestParam(value = "name", required = true) String name,
-	        @RequestParam(value = "ext", required = true) String ext,
-	        @RequestParam(value = "from", required = true) String from,
-	        @RequestParam(value = "ext", required = true) String to,
-	        @RequestParam(required = false) Map<String, String> params) throws JRException, IOException {
+			@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "ext", required = true) String ext,
+			@RequestParam(value = "from", required = true) String from,
+			@RequestParam(value = "to", required = true) String to,
+			@RequestParam(required = false) Map<String, String> params) throws JRException, IOException {
 
 		String filePath = generateJasperReport(name, ext, params);
 		String fileName = name + "." + ext;
@@ -109,7 +353,8 @@ public class ReportController {
 
 	}
 
-	public String generateJasperReport(String reportName, String extension, Map<String, String> params) throws JRException {
+	public String generateJasperReport(String reportName, String extension, Map<String, String> params)
+			throws JRException {
 
 		InputStream reportStream = getClass().getResourceAsStream("/rpt/" + reportName + ".jrxml");
 		JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
@@ -121,8 +366,7 @@ public class ReportController {
 		Connection conn;
 		try {
 			conn = connect(url);
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			log.error("Error connecting to DB.", e);
 			return "Error connecting to DB.";
 		}
@@ -172,7 +416,7 @@ public class ReportController {
 	 * @throws JRException
 	 */
 	private static void exportPDFFormat(JasperPrint jasperPrint, String exportPath, boolean pdfAutoPrint)
-	        throws JRException {
+			throws JRException {
 		JRPdfExporter exporter = new JRPdfExporter();
 		exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
 		exporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, exportPath);
@@ -204,8 +448,7 @@ public class ReportController {
 		// Step 1: Load the JDBC driver.
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-		}
-		catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			log.error("Could not find JDBC driver class.", e);
 			throw (SQLException) e.fillInStackTrace();
 		}
@@ -213,7 +456,8 @@ public class ReportController {
 		// Step 2: Establish the connection to the database.
 		String username = Context.getRuntimeProperties().getProperty("connection.username");
 		String password = Context.getRuntimeProperties().getProperty("connection.password");
-		log.debug("connecting to DATABASE: " + OpenmrsConstants.DATABASE_NAME + " USERNAME: " + username + " URL: " + url);
+		log.debug("connecting to DATABASE: " + OpenmrsConstants.DATABASE_NAME + " USERNAME: " + username + " URL: "
+				+ url);
 		return DriverManager.getConnection(url, username, password);
 	}
 
@@ -240,17 +484,58 @@ public class ReportController {
 		Date date;
 		try {
 			date = simpleDateFormat.parse(string);
-		}
-		catch (ParseException e) {
+		} catch (ParseException e) {
 			try {
 				simpleDateFormat = new SimpleDateFormat(detectDateFormat(string));
 				date = simpleDateFormat.parse(string);
-			}
-			catch (ParseException e2) {
+			} catch (ParseException e2) {
 				log.error(e2.getMessage());
 				return null;
 			}
 		}
 		return date;
 	}
+
+	public static class AliasToEntityOrderedMapResultTransformer extends AliasedTupleSubsetResultTransformer {
+
+		public final static AliasToEntityOrderedMapResultTransformer INSTANCE = new AliasToEntityOrderedMapResultTransformer();
+
+		/**
+		 * Disallow instantiation of AliasToEntityOrderedMapResultTransformer .
+		 */
+		private AliasToEntityOrderedMapResultTransformer() {
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		public Object transformTuple(Object[] tuple, String[] aliases) {
+			/* please note here LinkedHashMap is used so hopefully u ll get ordered key */
+			Map result = new LinkedHashMap(tuple.length);
+			for (int i = 0; i < tuple.length; i++) {
+				String alias = aliases[i];
+				if (alias != null) {
+					result.put(alias, tuple[i]);
+				}
+			}
+			return result;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		public boolean isTransformedValueATupleElement(String[] aliases, int tupleLength) {
+			return false;
+		}
+
+		/**
+		 * Serialization hook for ensuring singleton uniqueing.
+		 * 
+		 * @return The singleton instance : {@link #INSTANCE}
+		 */
+		private Object readResolve() {
+			return INSTANCE;
+		}
+	}
+
 }
